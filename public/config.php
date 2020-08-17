96fd7421c626ae8b07777e08d0df23d3cc370aa0
Ali Baver Kahraman <alibaverkahraman@Ali-MacBook-Pro.local>
2020-08-17 19:00:24 +0300

    asd

--- public/config.php
+++ public/config.php
@@ -1,15 +1,16 @@
 <?php
-// $host = "127.0.0.1";
-// $user = "root";
-// $pass = "";
-// $dbname = "iste_library";
+$host = "127.0.0.1";
+$user = "root";
+$pass = "";
+$dbname = "iste_library";
 
-$host = "zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user = "yt175solm61xhx2e";
-$pass = "wryysgbmub9ro2sx";
-$dbname = "l0v09yzxdjr8ms7s";
+// $host = "zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+// $user = "yt175solm61xhx2e";
+// $pass = "wryysgbmub9ro2sx";
+// $dbname = "l0v09yzxdjr8ms7s";
+// 3306
 
-$con = mysqli_connect($host, $user, $pass, $dbname, "3306");
+$con = mysqli_connect($host, $user, $pass, $dbname);
 
 if (!$con)
     die("Connection failed: " . mysqli_connect_error());
